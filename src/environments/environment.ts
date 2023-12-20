4796a67b69b97871c96f0280cf0a1c537c4d42f0
pranav3996 <yadavpranav456@gmail.com>
2023-12-20 12:04:04 +0530

    20-12-2023

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-export const environment = {
-    production: true,
-    Client_ID:
-      'AfRdDCfUoQG-FZTY6z3-02s2RPbvn37HWjp_8J3OsAYhFGLYFgENC_a2GQTHb_l1S9u_uL2yazopBxgv',
-  };
+// export const environment = {
+//     production: true,
+//     Client_ID:
+//       'AfRdDCfUoQG-FZTY6z3-02s2RPbvn37HWjp_8J3OsAYhFGLYFgENC_a2GQTHb_l1S9u_uL2yazopBxgv',
+//   };
   
